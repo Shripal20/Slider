eaa7e6612862b3c5a19e49ad99d05dc367fd0018
Shripal <shripal@gmail.com>
2023-06-04 15:22:58 +0530

    Add file

--- src/Alternative.js
+++ src/Alternative.js
@@ -6,21 +6,46 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, people]);
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
+
+  //   useEffect(() => {
+  //     const lastIndex = people.length - 1;
+  //     if (index < 0) {
+  //       setIndex(lastIndex);
+  //     }
+  //     if (index > lastIndex) {
+  //       setIndex(0);
+  //     }
+  //   }, [index, people]);
 
   useEffect(() => {
-   let slider = setInterval(() => {
-      setIndex(index + 1);
-    }, 3000);
-    return ()=> clearInterval(slider)
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0
+        }
+        return index;
+      });
+    }, 5000);
+    return () => clearInterval(slider);
   }, [index]);
   return (
     <section className="section">
@@ -53,10 +78,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => setIndex(index + 1)}>
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
